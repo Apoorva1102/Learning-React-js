affa5ab15046b3befe401ab8a9ed463611091cea
Apoorva1102 <apoorvamaurya1102@gmail.com>
2026-07-13 12:09:00 +0530

    Learned React Router

---
+++ 07reactRouter/src/components/contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+export default function Contact(){
+    return(
+        
+        <>
+        <div>
+        <h1 className="text-center text-2xl text-orange-700"> Contact us on our for more information!</h1>
+        <img className=" mx-auto my-4 rounded-2xl w-50 h-50 align-middle flex"
+        src="https://images.pexels.com/photos/18920438/pexels-photo-18920438.jpeg" 
+        />
+</div>
+        </>
+    )
+}
